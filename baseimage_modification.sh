6584fea23cdd402d49af91b918fcb0534db08d80
m-fariz-a <falasyir@yahoo.com>
2023-07-02 16:24:29 +0700

    remove app from base image layering

--- baseimage_modification.sh
+++ baseimage_modification.sh
@@ -8,8 +8,4 @@ unrar \
 gnome-shell-extension-appindicator \
 gnome-shell-extension-caffeine \
 gnome-shell-extension-dash-to-dock \
-gnome-shell-extension-gsconnect \
-gnome-shell-extension-just-perfection \
-gnome-shell-extension-system-monitor-applet \
-gnome-shell-extension-user-theme \
-yaru-theme
+gnome-shell-extension-gsconnect
