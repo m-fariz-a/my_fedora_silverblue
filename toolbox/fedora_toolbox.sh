1227bdb828039c416cc82fb75e2f438a9df36970
m-fariz-a <falasyir@yahoo.com>
2023-09-24 20:52:34 +0700

    update toolbox script

--- toolbox/fedora_toolbox.sh
+++ toolbox/fedora_toolbox.sh
@@ -1,9 +1,17 @@
-sudo dnf update -y
+# update container
+sudo dnf update -y &&
+
+# install app
 sudo dnf install -y \
 htop \
 neofetch \
 python3-pip \
-python3.11 \
 python3.9 \
 R \
-rstudio-desktop
+rstudio-desktop &&
+
+# setup basic python libs
+pip install spyder jupyterlab pandas xlrd openpyxl seaborn &&
+mkdir ~/pyenv &&
+python3.9 -m venv ~/pyenv/py-testing &&
+
